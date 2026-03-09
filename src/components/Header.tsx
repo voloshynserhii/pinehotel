9e6d6efb11f077187977860995765f9dac3d0dd7
Serhii Voloshyn <vosquery@gmail.com>
2026-03-09 19:22:15 +0100

    move the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ export function Header() {
           href={`/${lang}`}
           className="text-white text-xl tracking-wide"
         >
-          <Image src="/pineslogo.png" alt="The Pines Hotel" width={350} height={300} className='w-[280px] h-[245px] lg:w-[350px] lg:h-[300px] transform translate-y-[70px] lg:translate-y-[85px]' />
+          <Image src="/pineslogo.png" alt="The Pines Hotel" width={350} height={300} className='w-[280px] h-[245px] lg:w-[350px] lg:h-[300px] transform translate-y-[70px] translate-x-[-80px] lg:translate-x-0 lg:translate-y-[85px]' />
         </Link>
 
         <Link
